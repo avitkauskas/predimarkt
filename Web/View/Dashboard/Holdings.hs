b0ba735c44584aca6918670ca072015535644b32
Alvydas Vitkauskas <alvydas.vitkauskas@gmail.com>
2026-02-01 17:54:59 +0200

    holdings case changes

--- Web/View/Dashboard/Holdings.hs
+++ Web/View/Dashboard/Holdings.hs
@@ -1,7 +1,7 @@
 module Web.View.Dashboard.Holdings where
 
-import Web.View.Prelude
 import Web.Types.Money
+import Web.View.Prelude
 
 data HoldingsView = HoldingsView { holdings :: [Include' ["marketId", "assetId"] Holding] }
 
@@ -12,21 +12,21 @@ instance View HoldingsView where
             {forEach holdings renderHolding}
         </div>
     |]
-            
+
 renderHolding :: (?context :: ControllerContext) => Include' ["marketId", "assetId"] Holding -> Html
-renderHolding holding = 
+renderHolding holding =
     let asset = holding.assetId
         market = holding.marketId
         money = formatMoney $ moneyFromCents (abs holding.amountCents)
-        profit = 
-            if holding.amountCents < 0 
+        profit =
+            if holding.amountCents < 0
                 then [hsx|profit of {money}|]
                 else [hsx|loss of {money}|]
-        position = 
+        position =
             case holding.quantity of
                 0 -> [hsx|closed : {profit}|]
                 n | n < 0 -> [hsx|open : short : {show (abs n)} shares : {money}|]
-                n | n > 0 -> [hsx|open : long : {show n} shares : {money}|]
+                _ -> [hsx|open : long : {show holding.quantity} shares : {money}|]
     in [hsx|
         <div class="card shadow-sm mb-3">
             <div class="card-header">
