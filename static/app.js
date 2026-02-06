f4e2c472dda7857ccb0d470f5965fcd81446b2f5
Alvydas Vitkauskas <alvydas.vitkauskas@gmail.com>
2026-02-06 22:06:41 +0200

    no need for custom flatpickr init

--- static/app.js
+++ static/app.js
@@ -28,28 +28,28 @@ $(document).on('ready turbolinks:load', function () {
 //     _hyperscript.processNode(document.body);
 // });
 
-window.initDatePicker = function () {
-    if (!('flatpickr' in window)) {
-        return;
-    }
+// window.initDatePicker = function () {
+//     if (!('flatpickr' in window)) {
+//         return;
+//     }
 
-    document.querySelectorAll("input[type='date']").forEach(el => {
-        flatpickr(el, {
-            ...(el.dataset.altFormat ? {} : { altFormat: 'd.m.y' }),
-            ...(el.dataset.altInput ? {} : { altInput: true }),
-        });
-    });
+//     document.querySelectorAll("input[type='date']").forEach(el => {
+//         flatpickr(el, {
+//             ...(el.dataset.altFormat ? {} : { altFormat: 'd.m.y' }),
+//             ...(el.dataset.altInput ? {} : { altInput: true }),
+//         });
+//     });
 
-    document.querySelectorAll("input[type='datetime-local']").forEach(el => {
-        flatpickr(el, {
-            ...(el.dataset.enableTime ? {} : { enableTime: true }),
-            ...(el.dataset.time_24hr ? {} : { time_24hr: true }),
-            ...(el.dataset.dateFormat ? {} : { dateFormat: 'Z' }),
-            ...(el.dataset.altFormat ? {} : { altFormat: 'd.m.y, H:i' }),
-            ...(el.dataset.altInput ? {} : { altInput: true }),
-        });
-    });
-}
+//     document.querySelectorAll("input[type='datetime-local']").forEach(el => {
+//         flatpickr(el, {
+//             ...(el.dataset.enableTime ? {} : { enableTime: true }),
+//             ...(el.dataset.time_24hr ? {} : { time_24hr: true }),
+//             ...(el.dataset.dateFormat ? {} : { dateFormat: 'Z' }),
+//             ...(el.dataset.altFormat ? {} : { altFormat: 'd.m.y, H:i' }),
+//             ...(el.dataset.altInput ? {} : { altInput: true }),
+//         });
+//     });
+// }
 
 window.toggleAssetForm = function (assetId, type) {
     const buyForm = document.getElementById(`buy-form-${assetId}`);
